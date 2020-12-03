bf0300f884b4360e57304fa0dfefce53035351a7
ggroshansii <garthgroshansii@gmail.com>
2020-12-02 20:16:03 -0500

    first coding challenge

---
+++ FizzBuzz.js
@@ -0,0 +1,28 @@
+/*
+Return an array containing the numbers from 1 to N, where N is the parametered value. N will never be less than 1
+
+Replace certain values however if any of the following conditions are met:
+
+If the value is a multiple of 3: use the value 'Fizz' instead
+If the value is a multiple of 5: use the value 'Buzz' instead
+If the value is a multiple of 3 & 5: use the value 'FizzBuzz' instead
+*/
+
+// Return an array
+
+function fizzbuzz(n) {
+  let updated_arr = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      updated_arr.push("FizzBuzz");
+    } else if (i % 3 == 0) {
+      updated_arr.push("Fizz");
+    } else if (i % 5 == 0) {
+      updated_arr.push("Buzz");
+    } else {
+      updated_arr.push(i);
+    }
+  }
+  return updated_arr;
+}
