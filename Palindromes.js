e59b3f05e4d3c08f9106f4bae901c0e3b8f4f830
ggroshansii <garthgroshansii@gmail.com>
2021-01-07 22:01:47 -0500

    coding challenge 6

---
+++ Palindromes.js
@@ -0,0 +1,22 @@
+/* 
+
+Write a function that determines whether or not a given string is a palindrome.
+
+A palindrome is a string that is spelled the same both forwards and backwards, usually without considering punctuation or word breaks
+
+*/
+
+function palindromes(word){
+
+    splitWord = word.split("");
+    wordCopy = splitWord.slice().reverse();
+
+    for (let i = 0; i < splitWord.length; i++){
+        if (splitWord[i] !== wordCopy[i]){
+            return false;
+        } else {
+            continue;
+        }
+    }
+    return true;
+}
