8f6e30ca3eea572f11636dca3446cafba8fa3d63
ggroshansii <garthgroshansii@gmail.com>
2021-02-18 20:25:37 -0500

    coding challenge #16

---
+++ SliceAndSplice.js
@@ -0,0 +1,25 @@
+/*
+
+You are given two arrays and an index.
+
+Copy each element of the first array into the second array, in order.
+
+Begin inserting elements at index n of the second array.
+
+Return the resulting array. The input arrays should remain the same after the function runs.
+
+*/
+
+
+
+function frankenSplice(arr1, arr2, n) {
+   
+    let localArray = arr2.slice();
+
+    for (let i  = 0; i < arr1.length; i++){
+            localArray.splice(n + i, 0, arr1[i]);
+
+  }
+  return localArray;
+}
+  
